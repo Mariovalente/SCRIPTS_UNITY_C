27c65463bb8a22db2290f67fca2d9eaabe8cba55
Mario Valente <84289809+Mariovalente@users.noreply.github.com>
2022-01-31 17:58:46 -0300

    Update Setup_movimento.cs

--- Setup_movimento.cs
+++ Setup_movimento.cs
@@ -2,18 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class variaveis : MonoBehaviour
-{ 
-    public float vel = 0.1f;
-    public Renderer quad; 
-    // Start is called before the first frame update
+public class PlayerController : MonoBehaviour
+{
+    public Animator playerAnimator;
+    float input_x = 0;
+    float input_y = 0;
+    public float speed = 2.5f;
+    bool IsWalking = false;
     void Start()
     {
-    
+        IsWalking = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 offset = new Vector2 (vel * Time.deltaTime, 0);
-        quad.material.mainTextureOffset += offset;
+        input_x = Input.GetAxisRaw("Horizontal");
+        input_y = Input.GetAxisRaw("Vertical");
+        IsWalking = (input_x != 0 || input_y != 0);
+
+        if(IsWalking)
+        {
+            var move = new Vector3(input_x,input_y,0).normalized;
+            transform.position += move * speed * Time.deltaTime; 
+            playerAnimator.SetFloat("Input_x", input_x);
+            playerAnimator.SetFloat("Input_y", input_y);
+
+        }
+        playerAnimator.SetBool("IsWalking",IsWalking);
+
+            if(Input.GetButtonDown("Fire1"))
+            {
+                playerAnimator.SetTrigger("Attack");
+            }
+
+    }
+
+}
